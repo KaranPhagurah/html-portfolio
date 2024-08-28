6ccce2af30d02e805f1843d95f2e3870d9429944
KaranPhagurah <87574751+KaranPhagurah@users.noreply.github.com>
2024-08-28 06:29:48 +0000

    Added Reset Button

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let counter = document.getElementById('counter');
 let count = document.getElementById('count');
 
+let clear = document.getElementById('clear');
+
 let c = 0;
 
-count.innerText = 'The button was clicked ${C} times!';
+count.innerText = `The button was clicked ${c} times!`;
 counter.addEventListener('click', () => {
     c++;
-    count.innerText = 'The button was clicked ${C} times!';
+    count.innerText = `The button was clicked ${c} times!`;
+})
+
+clear.addEventListener("click", () => {
+    c = 0;
+    count.innerText = `The button was clicked ${c} times!`; 
 })
